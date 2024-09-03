aac87f8ee513f66e77ebc96d0365b06512c302de
zlajaa1 <vujicic.zlatko@gmail.com>
2024-09-03 18:53:56 +0200

    contact section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,12 +9,12 @@
           <span class="highlight">Frontend</span>
           <br>developer
         </h1>
-        <p class="text-xl md:text-3xl">
+        <p class="text-xl md:text-3xl mb-8">
           <span class="underlined">+8 years</span> of Web development,  curious and playful
           in exploring the internet playground.
         </p>
-        <p class="contact">
-          You can contact me on <span class="highlight">vujicic.zlatko@gmail.com</span>
+        <p class="contact uppercase">
+          You can contact me on <a href="mailto:vujicic.zlatko@gmail.com" class="link">vujicic.zlatko@gmail.com</a>
         </p>
       </div>
     </div>
@@ -23,14 +23,6 @@
 
 <style scoped>
 .contact{
-  position: absolute;
-  bottom: 20px;
-  left: 0;
-  width: 100%;
   text-align: center;
-}
-.marked{
-  color: red;
-  background: yellow;
 }
 </style>
