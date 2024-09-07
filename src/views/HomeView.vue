97591a0967e885b62f900305cf9e64fc370eb165
zlajaa1 <vujicic.zlatko@gmail.com>
2024-09-07 18:35:03 +0200

    pdf donload and file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
+  <main class="home-page-wrapper">
     <div class="h-screen flex flex-col items-center justify-center relative">
       <div class="max-w-[1200px] mx-auto text-center">
         <p class="text-xl md:text-3xl mb-0">
@@ -16,6 +16,9 @@
         <p class="contact uppercase">
           You can contact me on <a href="mailto:vujicic.zlatko@gmail.com" class="link">vujicic.zlatko@gmail.com</a>
         </p>
+        <p>
+          or <a href="ZlatkoVujicic.pdf" class="link" download>Download resume</a>
+        </p>
       </div>
     </div>
   </main>
@@ -24,5 +27,14 @@
 <style scoped>
 .contact{
   text-align: center;
+}
+.home-page-wrapper{
+  padding: 0 20px 0 20px;
+}
+@media (min-width: 1024px) {
+  .home-page-wrapper{
+    width: 100%;
+    padding: 0 0 0 100px;
+  }
 }
 </style>
